2da3661a85a47bfab5e37f80f6bb14621300eadf
kgw4it <30734393+kgw4it@users.noreply.github.com>
2018-06-25 22:18:11 +0200

    derive domain from provided one

--- certbot-dns-easyname/certbot_dns_easyname/dns_easyname.py
+++ certbot-dns-easyname/certbot_dns_easyname/dns_easyname.py
@@ -198,9 +198,10 @@ class _EasyNameAPIClient(object):
 		Get the specified domain document
 		"""
 		domains = self.list_domains(100, 0)
+		zone_name_guesses = dns_common.base_domain_name_guesses(domain)
 		for domain in domains:
-			if domain['domain'] == domain_name:
-				return domain
+			if domain['domain'] in zone_name_guesses:
+				return domain['domain']
 				
 		raise errors.PluginError('Domain not found: {0}'.format(domain_name))
 	
